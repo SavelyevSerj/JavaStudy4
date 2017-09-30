cc4a767cb3d65d964fc04fb90cf2bf12848b48b1
SavelyevSerj <bigbadvoodoodaddy846@gmail.com>
2017-09-30 15:47:13 +0300

    I have added the sout with the "This is the changing of the projec."

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This project will be committed into the GitHub.");
+        System.out.println("This the changing of the project.");
     }
 }
